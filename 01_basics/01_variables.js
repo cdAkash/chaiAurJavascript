c5525f253598a7d1988cde7c8b82cd1635c4c260
akashyadav15032002 <akashyadav15032002@gmail.com>
2024-09-08 12:24:53 +0000

    FirstCommit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accId = 123;
+// const -> cannot be reassigned, you cannot change
+let accEmail = "Akash@gmail.com"
+// let -> can be reassigned, you can change
+var accPass = "12345";
+accCity = "jaipur" // it is also possible but not recommended
+let accName; //undefined
+/*
+Prefer not to use var, because of issuse in block scope and functional scope
+
+*/
+console.log(accId);
+console.log(accEmail);
+
+console.table([accId, accEmail, accPass, accCity,accName])
